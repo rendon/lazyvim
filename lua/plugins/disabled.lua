47cd4ac5fdc7c7ec0786a9fe44d8a9b42acf85d2
Rafael Rendon Pablo <rafaelrendonpablo@gmail.com>
2024-04-11 08:56:24 -0700

    Disable mini.pairs

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,7 @@
 return {
   -- disable trouble
   { "nvim-notify", enabled = false },
-  { "folke/flash.nvim", enabled = false },
-  { "nvimdev/dashboard-nvim", enabled = false },
+  { "flash.nvim", enabled = false },
+  { "dashboard-nvim", enabled = false },
+  { "mini.pairs", enabled = false },
 }
