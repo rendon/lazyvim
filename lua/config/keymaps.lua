b400bae8066da1c46bb95b73855baeb1116da6fb
Rafael Rendon Pablo <rafaelrendonpablo@gmail.com>
2024-04-20 11:56:26 -0700

    Update key mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -36,28 +36,29 @@ vim.keymap.set(
 )
 
 vim.keymap.set(
-  "i", "<C-K>", "<esc>vbUea",
+  "i", "<c-k>", "<esc>vbUea",
   { remap = true, desc = "Convert last word to caps" }
 )
 
+vim.keymap.set(
+  "i", "<c-c>", "```<cr>```<c-o>O",
+  { remap = true, desc = "Insert Markdown code block" }
+)
 
 vim.keymap.set(
-  "n", "<leader>a", ":NvimCmpToggle<CR>",
+  "n", "<leader>a", ":NvimCmpToggle<cr>",
   { remap = true, desc = "Toggle auto completion" }
 )
 
 -- Launch panel if nothing is typed after <leader>z
-vim.keymap.set("n", "<leader>z", "<cmd>Telekasten panel<CR>")
+vim.keymap.set("n", "<leader>z", "<cmd>Telekasten panel<cr>")
 
 -- Most used functions
-vim.keymap.set("n", "<leader>zf", "<cmd>Telekasten find_notes<CR>")
-vim.keymap.set("n", "<leader>zg", "<cmd>Telekasten search_notes<CR>")
-vim.keymap.set("n", "<leader>zd", "<cmd>Telekasten goto_today<CR>")
-vim.keymap.set("n", "<leader>zz", "<cmd>Telekasten follow_link<CR>")
-vim.keymap.set("n", "<leader>zn", "<cmd>Telekasten new_note<CR>")
-vim.keymap.set("n", "<leader>zc", "<cmd>Telekasten show_calendar<CR>")
-vim.keymap.set("n", "<leader>zb", "<cmd>Telekasten show_backlinks<CR>")
-vim.keymap.set("n", "<leader>zI", "<cmd>Telekasten insert_img_link<CR>")
+vim.keymap.set("n", "<leader>nn", "<cmd>Telekasten new_note<cr>")
+vim.keymap.set("n", "<leader>nf", "<cmd>Telekasten search_notes<cr>")
 
--- Call insert link automatically when we start typing a link
--- vim.keymap.set("i", "[[", "<cmd>Telekasten insert_link<CR>")
+vim.keymap.set("n", "<leader>zd", "<cmd>Telekasten goto_today<cr>")
+vim.keymap.set("n", "<leader>zz", "<cmd>Telekasten follow_link<cr>")
+vim.keymap.set("n", "<leader>zc", "<cmd>Telekasten show_calendar<cr>")
+vim.keymap.set("n", "<leader>zb", "<cmd>Telekasten show_backlinks<cr>")
+vim.keymap.set("n", "<leader>zI", "<cmd>Telekasten insert_img_link<cr>")
