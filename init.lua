cdcdf14ce70cc5317b98f97f0eab49821819a2a0
Rafael Rendon <rafaelrendonpablo@gmail.com>
2024-05-04 14:21:55 -0700

    Fix indentation in init.lua

--- init.lua
+++ init.lua
@@ -2,10 +2,12 @@
 require("config.lazy")
 
 require('telekasten').setup({
-  -- Put the name of your notes directory here
-  home = vim.fn.expand("~/projects/notes/"),
-  -- Put the name of your notes directory here
-  template_new_note = vim.fn.expand("~/projects/notes/.new-note-template"),
+    -- Put the name of your notes directory here
+    home = vim.fn.expand("~/projects/notes/"),
+    -- Put the name of your notes directory here
+    template_new_note = vim.fn.expand("~/projects/notes/.new-note-template"),
+    -- Replace spaces in the file name with dashes
+    filename_space_subst = '-',
 })
 
 require("lspconfig").marksman.setup({})
@@ -15,22 +17,22 @@ require('lspconfig').pyright.setup({})
 -- https://github.com/hrsh7th/nvim-cmp/issues/715#issuecomment-1806944224
 local timer = nil
 vim.api.nvim_create_autocmd({ "TextChangedI", "CmdlineChanged" }, {
-  pattern = "*",
-  callback = function()
-    if timer then
-      vim.loop.timer_stop(timer)
-      timer = nil
-    end
+    pattern = "*",
+    callback = function()
+        if timer then
+            vim.loop.timer_stop(timer)
+            timer = nil
+        end
 
-    timer = vim.loop.new_timer()
-    timer:start(1000, 0, vim.schedule_wrap(function()
-      require('cmp').complete({ reason = require('cmp').ContextReason.Auto })
-    end))
-  end
+        timer = vim.loop.new_timer()
+        timer:start(1000, 0, vim.schedule_wrap(function()
+            require('cmp').complete({ reason = require('cmp').ContextReason.Auto })
+        end))
+    end
 })
 
 
- -- globally
+-- globally
 vim.g.autoformat = false
 
 -- " Enable AutoSave on Vim startup
